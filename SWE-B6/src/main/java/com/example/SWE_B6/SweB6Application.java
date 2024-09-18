3d55b52bee78921b040bcc223cf86b75a1404c86
j40769 <122215145+j40769@users.noreply.github.com>
2024-09-18 17:42:43 -0400

    Add files via upload

---
+++ SWE-B6/src/main/java/com/example/SWE_B6/SweB6Application.java
@@ -0,0 +1,13 @@
+package com.example.SWE_B6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweB6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweB6Application.class, args);
+	}
+
+}
---
+++ SWE-B6/src/test/java/com/example/SWE_B6/SweB6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SWE_B6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweB6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
