3503d655f395c849dfe8edc49aca2bc4064258e6
znh05917 <znh05917@uga.edu>
2024-09-18 18:07:39 -0400

    Hello [Name]

--- SWE-B6/src/main/java/com/example/SWE_B6/SweB6Application.java
+++ SWE-B6/src/main/java/com/example/SWE_B6/SweB6Application.java
@@ -9,5 +9,8 @@ public class SweB6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SweB6Application.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
